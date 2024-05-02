627be646a3db2be13ed5823ecbd065e943a72f39
leandrogs2006 <128071897+leandrogs2006@users.noreply.github.com>
2024-05-02 14:40:44 -0300

    main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-09-28T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-31T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
-const tempoObjetivo4 = new Date("2024-09-28T00:00:00");
+const tempoObjetivo1 = new Date("2025-09-28T00:00:00");
+const tempoObjetivo2 = new Date("2029-12-31T00:00:00");
+const tempoObjetivo3 = new Date("2027-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2026-09-28T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
